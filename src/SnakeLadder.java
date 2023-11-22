bc83ab050016538db59d131ae55d7ab091b2fd52
Vaisakhkrishnan K <cb.en.u4cse20069@cb.students.amrita.edu>
2023-11-22 19:07:01 +0530

    Two Players Added

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -53,30 +53,57 @@ class Die{
 
 public class SnakeLadder {
 	
-	public static void playGame(Player p1, Die die) {
-		while(p1.getPosition() < 100) {
+	public static void playGame(Player p1, Player p2, Die die) {
+		int chance = 0;
+		while((p1.getPosition() < 100) && (p2.getPosition() < 100)) {
 			int roll = die.rollDie();
-			int move = p1.nextMove();
-			p1.setNoOfRolls();
-			if(move == 0) {
-				roll = -1 * roll;
-				p1.setPosition(roll);
-			}
-			else if(move == 1) {
-				//No Move
+			if(chance == 0) {
+				int move = p1.nextMove();
+				p1.setNoOfRolls();
+				if(move == 0) {
+					roll = -1 * roll;
+					p1.setPosition(roll);
+				}
+				else if(move == 1) {
+					//No Move
+				}
+				else {
+					p1.setPosition(roll);
+				}
+				chance = 1;
 			}
 			else {
-				p1.setPosition(roll);
+				int move = p2.nextMove();
+				p2.setNoOfRolls();
+				if(move == 0) {
+					roll = -1 * roll;
+					p2.setPosition(roll);
+				}
+				else if(move == 1) {
+					//No Move
+				}
+				else {
+					p2.setPosition(roll);
+				}
+				chance = 0;
 			}
+
 		}
-		System.out.println("PLayer Won");
-		System.out.println("No of die rolls to win: "+ p1.getNoOfRolls());
+		if(p1.getPosition() == 100) {
+			System.out.println("PLayer P1 Won");
+			System.out.println("No of die rolls to win: "+ p1.getNoOfRolls());
+		}
+		else {
+			System.out.println("PLayer P2 Won");
+			System.out.println("No of die rolls to win: "+ p2.getNoOfRolls());
+		}
 	}
 	
 	public static void main(String args[]) {
 		Die die = new Die();
 		Player p1 = new Player("Player 1");
-		playGame(p1, die);
+		Player p2 = new Player("Player 2");
+		playGame(p1, p2, die);
 	}
 	
 }
