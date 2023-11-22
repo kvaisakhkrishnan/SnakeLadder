447e76774cb9b785f011ad792649dbb06032a397
Vaisakhkrishnan K <cb.en.u4cse20069@cb.students.amrita.edu>
2023-11-22 18:39:43 +0530

    Roll Die Functionality

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -18,9 +18,21 @@ class Player{
 	}
 	
 }
+
+class Die{
+	public int rollDie() {
+		Random random = new Random();
+		int number = random.nextInt(6) + 1;
+		System.out.println("Die rolled, result: " + number);
+		return number;
+	}
+}
+
 public class SnakeLadder {
 	public static void main(String args[]) {
+		Die die = new Die();
 		Player p1 = new Player("Player 1");
+		die.rollDie();
 	}
 	
 }
